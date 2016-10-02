68954db6a39a31c7eda6656397e627db549bdfb2
Tatyana Alexeenkova <talexeenkova@edmunds.com>
2016-10-02 02:16:48 +0200

    add host

--- public/scripts/ws-client.js
+++ public/scripts/ws-client.js
@@ -1,7 +1,7 @@
 if (!window.WebSocket) {
     document.body.innerHTML = 'WebSocket is not supported in this browser.';
 }
-var host = NODE_ENV === 'production' ? 'nodejswschat.herokuapp.com' : 'localhost';
+var host = process.env.NODE_ENV === 'production' ? 'nodejswschat.herokuapp.com' : 'localhost';
 var socket = new WebSocket('ws://' + host + ':3001');
 var helper = require('./event-handler')(socket);
 var clientsHelper = require('./clients')();
